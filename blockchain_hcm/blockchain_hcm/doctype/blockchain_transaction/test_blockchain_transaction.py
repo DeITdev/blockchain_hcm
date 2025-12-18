460cba5b9ff6cde8b5a2d9af13a41be4111ae0be
DeITdev <danarikram@gmail.com>
2025-12-19 00:43:16 +0700

    fix erros update

---
+++ blockchain_hcm/blockchain_hcm/doctype/blockchain_transaction/test_blockchain_transaction.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2025, Danar IT and Contributors
+# See license.txt
+
+import frappe
+from frappe.tests import IntegrationTestCase, UnitTestCase
+
+
+class UnitTestBlockchainTransaction(UnitTestCase):
+	"""Unit tests for BlockchainTransaction."""
+	pass
+
+
+class IntegrationTestBlockchainTransaction(IntegrationTestCase):
+	"""Integration tests for BlockchainTransaction."""
+	pass
---
+++ blockchain_hcm/blockchain_hcm/doctype/smart_contract/test_smart_contract.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2025, Danar IT and Contributors
+# See license.txt
+
+import frappe
+from frappe.tests import IntegrationTestCase, UnitTestCase
+
+
+class UnitTestSmartContract(UnitTestCase):
+	"""Unit tests for SmartContract."""
+	pass
+
+
+class IntegrationTestSmartContract(IntegrationTestCase):
+	"""Integration tests for SmartContract."""
+	pass
